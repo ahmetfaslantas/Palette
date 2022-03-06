be4fa200a61e66b6bd710a6663e9d75b79ef29a3
Ahmet Faruk ASLANTAŞ <ahmetf.aslantas@gmail.com>
2022-03-06 19:20:58 +0300

    POST endpoints for announcement and assignment added.

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -37,4 +37,42 @@ router.post("/newcourse", authVerify, async (req, res) => {
     res.status(200).send({ message: "Course created" });
 });
 
+router.post("/:id/announcement", authVerify, async (req, res) => {
+    if (res.locals.role === "student") {
+        return res.status(400).send({ error: "Students cannot create announcements" });
+    }
+
+    const { title, content } = req.body;
+
+    const course = await Course.findById(req.params.id);
+
+    if (!course) {
+        return res.status(400).send({ error: "Course not found" });
+    }
+
+    course.announcements.push({ title: title, content: content });
+    await course.save();
+
+    res.status(200).send({ message: "Announcement created" });
+});
+
+router.post("/:id/assignment", authVerify, async (req, res) => {
+    if (res.locals.role === "student") {
+        return res.status(400).send({ error: "Students cannot create assignments" });
+    }
+
+    const { title, content, files } = req.body;
+
+    const course = await Course.findById(req.params.id);
+
+    if (!course) {
+        return res.status(400).send({ error: "Course not found" });
+    }
+
+    course.assignments.push({ title: title, content: content, files: files });
+    await course.save();
+
+    res.status(200).send({ message: "Assignment created" });
+});
+
 module.exports = router;
