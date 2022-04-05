0fbfd0f4cef18336fa0c4eb6d9ae8e9762cb40da
Ahmet Faruk ASLANTAŞ <ahmetf.aslantas@gmail.com>
2022-04-05 17:11:11 +0300

    Files are now saved to Docker container volumes.

--- src/routes/files.js
+++ src/routes/files.js
@@ -8,20 +8,22 @@ const router = express.Router();
 const upload = multer({
     storage: multer.diskStorage({
         destination: (req, file, cb) => {
-            let path = `./uploads/students/${req.res.locals.userId}/`;
+            let path = `${process.env.UPLOAD_ROOT}/uploads/students/${req.res.locals.userId}/`;
+
             if (!fs.existsSync(path)) {
                 fs.mkdirSync(path, { recursive: true });
             }
+
             cb(null, path);
         },
         filename: (req, file, cb) => {
             let edition = 1;
             let extension = file.originalname.split(".").pop();
             let filename = file.originalname.split(".").slice(0, -1).join(".");
-            let path = `./uploads/students/${req.res.locals.userId}/${file.originalname}`;
+            let path = `${process.env.UPLOAD_ROOT}/uploads/students/${req.res.locals.userId}/${file.originalname}`;
             let name = file.originalname;
             while (fs.existsSync(path)) {
-                path = `./uploads/students/${req.res.locals.userId}/${filename}_${edition}.${extension}`;
+                path = `${process.env.UPLOAD_ROOT}/uploads/students/${req.res.locals.userId}/${filename}_${edition}.${extension}`;
                 name = `${filename}_${edition}.${extension}`;
                 edition++;
             }
