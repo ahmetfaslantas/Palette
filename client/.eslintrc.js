ebaadee44827c73c9b9ecac8b3db1b501f391b59
Ahmet Faruk ASLANTAŞ <ahmetf.aslantas@gmail.com>
2022-05-22 23:06:26 +0300

    Simple fixes and linting rule updates.

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -24,6 +24,9 @@ module.exports = {
     "indent": [
       "error",
       2,
+      {
+        "SwitchCase": 1,
+      },
     ],
     "react/jsx-indent": [
       "error",
--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -2,28 +2,28 @@ import { useReducer, useCallback } from "react";
 
 function reducer(state, action) {
   switch (action.type) {
-  case "FETCH_INIT":
-    return {
-      ...state,
-      isLoading: true,
-      isError: false,
-    };
-  case "FETCH_SUCCESS":
-    return {
-      ...state,
-      isLoading: false,
-      isError: false,
-      done: true,
-      data: action.payload,
-    };
-  case "FETCH_FAILURE":
-    return {
-      ...state,
-      isLoading: false,
-      isError: true,
-    };
-  default:
-    throw new Error();
+    case "FETCH_INIT":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case "FETCH_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        done: true,
+        data: action.payload,
+      };
+    case "FETCH_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    default:
+      throw new Error();
   }
 }
 
@@ -40,25 +40,30 @@ function useFetch(
     done: false,
   });
 
-  const fetchData = useCallback(async (body) => {
-    dispatch({ type: "FETCH_INIT" });
-    const response = await fetch(`${process.env.API_URL}${url}`, {
-      method: options.method,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: body ? JSON.stringify(body) : null,
-      credentials: "include",
-    });
-    const data = await response.json();
+  const fetchData = useCallback(
+    async (body) => {
+      dispatch({ type: "FETCH_INIT" });
 
-    if (!response.ok) {
-      dispatch({ type: "FETCH_FAILURE", payload: data });
-      return;
-    }
+      const response = await fetch(`${process.env.API_URL}${url}`, {
+        method: options.method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: body ? JSON.stringify(body) : null,
+        credentials: "include",
+      });
 
-    dispatch({ type: "FETCH_SUCCESS", payload: data });
-  }, [url, options]);
+      if (!response.ok) {
+        dispatch({ type: "FETCH_FAILURE", payload: data });
+        return;
+      }
+
+      const data = await response.json();
+
+      dispatch({ type: "FETCH_SUCCESS", payload: data });
+    },
+    [url, options]
+  );
 
   return {
     ...state,
